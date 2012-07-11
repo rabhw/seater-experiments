800e4782c2c019534d1151285ec1da16832fc190
Ross Lavery <rosslavery@gmail.com>
2012-07-11 14:38:43 -0400

    First commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/* Author:
+
+*/
+
+
+$(document).ready(function() {
+
+	$('.rotate-table').on('click', function(e) {
+
+		var $this = $(this);
+		var $table = $this.parent().parent().find('.table');
+
+		if ($this.hasClass('cw')) {
+			$table.transition({ rotate: '+=45deg' });
+		}
+
+		else {
+			$table.transition({ rotate: '-=45deg' });
+		}
+
+		e.preventDefault();
+	});
+
+});
