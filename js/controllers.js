7d4451faa8f0b7ea4d1ecdbbb677855adbde4fd6
Ross Lavery <rosslavery@gmail.com>
2012-07-18 12:46:57 -0400

    Code commenting

--- js/controllers.js
+++ js/controllers.js
@@ -65,21 +65,26 @@ function EditSeatCtrl($scope, $routeParams) {
 		console.log('adding guest');
 	}
 
-
+	// Get the current table based on route tableId
 	var table = _.filter($scope.plans.tables, function(obj) {
 		return obj.id == $routeParams.tableId;
 	});
 
 	$scope.table = table[0];
 
+
+	// Do a loopup in the seats object for id that matches route seatId
 	var guestId = $scope.table.seats[$routeParams.seatId];
 
+	// Get the current guest based on route
 	var guest = _.filter($scope.plans.guests, function(obj) {
 		return obj.id == guestId;
 	});
 
 	$scope.guest = guest[0];
 
+
+	// TODO: Convert guest name field into token input
 	$('#guest-name').tokenInput($scope.plans.guests, {
 		tokenLimit: 1,
 		prePopulate: [$scope.guest]
