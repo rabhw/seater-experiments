d9af92d52f7f0d1e79671d20c55fa3e4fa78bd3f
Ross Lavery <rosslavery@gmail.com>
2012-07-28 12:49:54 -0400

    Draggable / Droppable

--- js/directives.js
+++ js/directives.js
@@ -3,30 +3,24 @@
 /* Directives */
 
 
-angular.module('seater.directives', []).directive('draggable', function($document) {
-  var startX=0, startY=0, x=0, y=0;
-  return function(scope, element, attr) {
-    element.css({
-      'position':'relative',
-      'border':'solid 1px red',
-      'background-color':'lightgrey',
-      'cursor':'pointer',
-    });
-    element.on('mousedown', function(e) {
-      startX = e.screenX - x;
-      startY = e.screenY - y;
-      $document.on('mousemove', function(e) {
-        y = e.screenY - startY;
-        x = e.screenX - startX;
-        element.css({
-          'top': y+'px',
-          'left': x+'px',
+angular.module('seater.directives', []).directive('draggable', function() {
+    return {
+        restrict:'A',
+        link:function(scope,element,attrs){
+           element.draggable({
+            containment: "parent"
+           });
+        }
+    };
+}).directive('droppable',function(){
+    return{
+      restrict:'A',
+      link:function(scope,element,attrs){
+        element.droppable({
+          drop:function(event,ui){
+            $( this ).addClass( "ui-state-highlight" ).find( "p" ).html( "Dropped!" );
+          }
         });
-      });
-      $document.on('mouseup', function(e) {
-        $document.off('mousemove');
-        $document.off('mouseup');
-      });
-    })
-  };
+      }
+    };
 });
